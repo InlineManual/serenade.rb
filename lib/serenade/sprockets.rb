ebb9158cc23ad98eef4c5ce9d437362c01bb1689
Vojtech Kusy <wojtha@gmail.com>
2019-01-18 21:36:06 +0100

    Add support for Sprockets 3 and 4: use register_compressor instead of register_engine
    DEPRECATION WARNING: Sprockets method `register_engine` is deprecated.
    Please register a mime type using `register_mime_type` then use `register_compressor` or `register_transformer`.
    https://github.com/rails/sprockets/blob/master/guides/extending_sprockets.md#supporting-all-versions-of-sprockets-in-processors

--- lib/serenade/sprockets.rb
+++ lib/serenade/sprockets.rb
@@ -1,4 +1,17 @@
 require "serenade/template"
 require "sprockets"
 
-Sprockets.register_engine(".serenade", Serenade::Template)
+# Sprockets 4
+# See https://github.com/rails/sprockets/blob/master/guides/extending_sprockets.md#registering-all-versions-of-sprockets-in-processors
+if Sprockets.respond_to?(:register_transformer)
+  Sprockets.register_mime_type "text/x-serenade", extensions: [".serenade"], charset: :javascript
+  Sprockets.register_preprocessor "text/x-serenade", Serenade::Template
+end
+
+# Sprockets 2, 3
+# See https://github.com/rails/sprockets/blob/master/guides/extending_sprockets.md#registering-all-versions-of-sprockets-in-processors
+if Sprockets.respond_to?(:register_engine)
+  args = [".serenade", Serenade::Template]
+  args << { mime_type: "text/x-serenade", silence_deprecation: true } if Sprockets::VERSION.start_with?("3")
+  Sprockets.register_engine(*args)
+end
