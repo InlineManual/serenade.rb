f15abe8168e4c9dd878e6c01018cefa5920d09eb
Vojtech Kusy <wojtha@gmail.com>
2019-01-18 23:02:31 +0100

    Replace rspec 'should' with 'expect'

--- spec/sprockets_spec.rb
+++ spec/sprockets_spec.rb
@@ -2,13 +2,13 @@ require "serenade/sprockets"
 
 describe "Serenade sprockets integration" do
   it "registers the .serenade template engine" do
-    Sprockets.engines[".serenade"].should eq Serenade::Template
+    expect( Sprockets.engines[".serenade"] ).to eq Serenade::Template
   end
 
   it "allows you to require serenade in your assets" do
     env = Sprockets::Environment.new
     env.append_path Serenade::ASSET_PATH
 
-    File.exists?(env.resolve "serenade").should be_true
+    expect( File.exist?(env.resolve "serenade") ).to eq true
   end
 end
